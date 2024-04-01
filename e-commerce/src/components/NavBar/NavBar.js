f455d69f67e31fea1a166c2e4a4364f051ad3838
Maheedhar Reddy Bukkasamudram <maheedharreddy.bukkasamudram@sjsu.edu>
2024-03-31 19:36:29 -0700

    checking icons

--- e-commerce/src/components/NavBar/NavBar.js
+++ e-commerce/src/components/NavBar/NavBar.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import
-    MenuOutlinedlined
-   from '@ant-design/icons';
+import ShoppingCartOutlined from '@ant-design/icons';
 
 const NavBar = () => {
   return (
-    <div className='px-6 py-4 flex justify-between items-center bg-blue-400'>
+    <div className='px-16 py-4 flex justify-between items-center bg-blue-400'>
              <div>E-cart</div>
-             <div><MenuOutlinedlined /></div>
+             <div className='flex items-center'>
+             <div>Profile</div>
+             <div className='mx-4'>Menu</div>
+             <div><ShoppingCartOutlined /></div>
+             </div>
+
     </div>
   )
 }
